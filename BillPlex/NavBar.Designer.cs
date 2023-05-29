f0025c8be8276606fbeb60341aa2f867e014c00a
sadishchandru <sadish@sippailab.com>
2023-05-29 10:27:16 +0530

    Icone Change In NavBar

--- BillPlex/NavBar.Designer.cs
+++ BillPlex/NavBar.Designer.cs
@@ -29,6 +29,7 @@ namespace BillPlex
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NavBar));
             this.ribbon = new DevExpress.XtraBars.Ribbon.RibbonControl();
             this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
             this.barSubItem1 = new DevExpress.XtraBars.BarSubItem();
@@ -44,6 +45,9 @@ namespace BillPlex
             // 
             // ribbon
             // 
+            // 
+            // 
+            // 
             this.ribbon.ExpandCollapseItem.Id = 0;
             this.ribbon.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
             this.ribbon.ExpandCollapseItem,
@@ -57,7 +61,7 @@ namespace BillPlex
             this.ribbonPageCategory1});
             this.ribbon.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
             this.ribbonPage1});
-            this.ribbon.Size = new System.Drawing.Size(588, 158);
+            this.ribbon.Size = new System.Drawing.Size(1055, 158);
             this.ribbon.StatusBar = this.ribbonStatusBar;
             this.ribbon.Click += new System.EventHandler(this.ribbon_Click);
             // 
@@ -79,7 +83,7 @@ namespace BillPlex
             this.ribbonPageCategory1.Name = "ribbonPageCategory1";
             this.ribbonPageCategory1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
             this.ribbonPage2});
-            this.ribbonPageCategory1.Text = "ribbonPageView";
+            this.ribbonPageCategory1.Text = "BillPlex";
             // 
             // ribbonPage2
             // 
@@ -113,22 +117,22 @@ namespace BillPlex
             // 
             // ribbonStatusBar
             // 
-            this.ribbonStatusBar.Location = new System.Drawing.Point(0, 395);
+            this.ribbonStatusBar.Location = new System.Drawing.Point(0, 563);
             this.ribbonStatusBar.Name = "ribbonStatusBar";
             this.ribbonStatusBar.Ribbon = this.ribbon;
-            this.ribbonStatusBar.Size = new System.Drawing.Size(588, 24);
+            this.ribbonStatusBar.Size = new System.Drawing.Size(1055, 24);
             // 
             // NavBar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(588, 419);
+            this.ClientSize = new System.Drawing.Size(1055, 587);
             this.Controls.Add(this.ribbonStatusBar);
             this.Controls.Add(this.ribbon);
+            this.IconOptions.Icon = ((System.Drawing.Icon)(resources.GetObject("NavBar.IconOptions.Icon")));
             this.Name = "NavBar";
             this.Ribbon = this.ribbon;
             this.StatusBar = this.ribbonStatusBar;
-            this.Text = "NavBar";
             ((System.ComponentModel.ISupportInitialize)(this.ribbon)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
